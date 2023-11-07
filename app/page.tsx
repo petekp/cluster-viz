4e0966b4be6f24accbf7e14566dc7ed9123cdfb6
Pete Petrash <pete.petrash@gmail.com>
2023-11-06 20:31:01 -0600

    stable checkpoint, pre-packing

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,46 @@
 "use client";
 
-import { useRef, useMemo } from "react";
 import * as THREE from "three";
-import { Canvas, useThree, useFrame } from "@react-three/fiber";
+import { useRef, useMemo } from "react";
 import { Leva, useControls } from "leva";
 import { InstancedMesh, SphereGeometry } from "three";
+import { Canvas, useThree, useFrame } from "@react-three/fiber";
 
-// Updating the Cluster component to resemble a TSNE map
-interface ClusterProps {
-  position: [number, number, number];
-  color: string;
-  nodeCount: number;
-  nodeDistance: number;
-  nodeSize: number;
-  minRadius: number;
-  maxRadius: number;
-}
+const CONTROL_SETTINGS = {
+  clusters: { value: 14, min: 4, max: 80, step: 2 },
+  diameter: { value: 0.75, min: 0.1, max: 100, step: 0.01 },
+  distance: { value: 1.4, min: 0.1, max: 10, step: 0.1 },
+  nodeCount: { value: 1000, min: 100, max: 10000, step: 20 },
+  nodeSize: { value: 0.001, min: 0.001, max: 0.2, step: 0.002 },
+};
 
-// Function to generate random positions for each node to resemble a cloud of points
+const COLORS = [
+  "red",
+  "green",
+  "blue",
+  "orange",
+  "purple",
+  "yellow",
+  "pink",
+  "cyan",
+  "magenta",
+  "lime",
+  "brown",
+  "white",
+  "gray",
+];
+
 function generateInitialPositions(
   nodeCount: number,
-  clusterRadius: number,
+  clusterDiameter: number,
   position: [number, number, number]
 ): [number, number, number][] {
+  const radius = clusterDiameter / 2;
   return Array.from({
     length: nodeCount,
   }).map((): [number, number, number] => {
-    const radius = clusterRadius;
-    const theta = Math.random() * 2 * Math.PI; // azimuthal angle
-    const phi = Math.acos(2 * Math.random() - 1); // polar angle
+    const theta = Math.random() * 2 * Math.PI;
+    const phi = Math.acos(2 * Math.random() - 1);
 
     // Convert spherical to Cartesian coordinates
     const x = position[0] + radius * Math.sin(phi) * Math.cos(theta);
@@ -39,59 +51,66 @@ function generateInitialPositions(
   });
 }
 
-// Function to scale the distance from the center of the cluster
+// Calculate the scale factor to fit the cluster into the clusterDiameter
 function scalePositions(
-  nodeDistance: number,
+  clusterDiameter: number,
   position: [number, number, number],
   initialPositions: [number, number, number][]
-) {
-  return initialPositions.map(([x, y, z]) => {
-    const dx = x - position[0];
-    const dy = y - position[1];
-    const dz = z - position[2];
+): [number, number, number][] {
+  const radius = Math.sqrt(
+    initialPositions.reduce((maxDist, [x, y, z]) => {
+      const dx = x - position[0];
+      const dy = y - position[1];
+      const dz = z - position[2];
+      return Math.max(maxDist, dx * dx + dy * dy + dz * dz);
+    }, 0)
+  );
 
-    const scale = nodeDistance;
-    return [
-      position[0] + dx * scale,
-      position[1] + dy * scale,
-      position[2] + dz * scale,
-    ];
-  });
+  const scale = clusterDiameter / (10 * radius);
+  return initialPositions.map(([x, y, z]) => [
+    position[0] + (x - position[0]) * scale,
+    position[1] + (y - position[1]) * scale,
+    position[2] + (z - position[2]) * scale,
+  ]);
 }
 
 function Cluster({
-  position,
-  nodeCount,
   color,
-  nodeDistance,
+  position,
+  diameter,
+  nodeCount,
   nodeSize,
-  minRadius,
-  maxRadius,
-}: ClusterProps) {
-  const clusterRadius = useRef(
-    Math.random() * (maxRadius - minRadius) + minRadius
-  );
+}: {
+  color: string;
+  position: [number, number, number];
+  diameter: number;
+  nodeCount: number;
+  nodeSize: number;
+}) {
+  const initialPositionsRef = useRef<[number, number, number][] | null>(null);
 
-  const initialPositions: [number, number, number][] = useMemo(
-    () => generateInitialPositions(nodeCount, clusterRadius.current, position),
-    [nodeCount, clusterRadius]
-  );
+  if (!initialPositionsRef.current) {
+    initialPositionsRef.current = generateInitialPositions(
+      nodeCount,
+      diameter,
+      position
+    );
+  }
 
   const positions = useMemo(
-    () => scalePositions(nodeDistance, position, initialPositions),
-    [nodeDistance, position, initialPositions]
+    () => scalePositions(diameter, position, initialPositionsRef.current!),
+    [diameter, position]
   );
 
   const meshRef = useRef<InstancedMesh>(null!);
   const matrix = new THREE.Matrix4();
+
   useFrame(() => {
     if (meshRef.current) {
       positions.forEach((pos, i) => {
-        meshRef.current.setMatrixAt(
-          i,
-          matrix.makeTranslation(...(pos as [number, number, number]))
-        );
+        meshRef.current.setMatrixAt(i, matrix.makeTranslation(...pos));
       });
+
       meshRef.current.instanceMatrix.needsUpdate = true;
     }
   });
@@ -109,51 +128,27 @@ function Cluster({
 }
 
 function Clusters() {
-  const { viewport } = useThree(); // Get viewport size
-  const settings = useControls("Settings", {
-    clusters: { value: 8, min: 4, max: 80, step: 2 },
-    nodeCount: { value: 1000, min: 100, max: 10000, step: 20 },
-    clusterDistance: { value: 0.2, min: 0.1, max: 0.3, step: 0.02 },
-    nodeDistance: { value: 0.15, min: 0.1, max: 1, step: 0.01 },
-    nodeSize: { value: 0.01, min: 0.001, max: 0.2, step: 0.002 },
-    minRadius: { value: 3, min: 1, max: 4, step: 1 },
-    maxRadius: { value: 7, min: 5, max: 7, step: 1 },
-  });
+  const { viewport } = useThree();
+  const settings = useControls("Settings", CONTROL_SETTINGS);
 
-  const colors = [
-    "red",
-    "green",
-    "blue",
-    "orange",
-    "purple",
-    "yellow",
-    "pink",
-    "cyan",
-    "magenta",
-    "lime",
-    "brown",
-    "white",
-    "gray",
-  ];
+  const scale = Math.min(viewport.width, viewport.height);
 
   return (
     <>
       {Array.from({ length: settings.clusters }).map((_, i) => {
-        const angle = (i / settings.clusters) * 2 * Math.PI; // Calculate angle around the circle
-        const clusterRadius = (settings.clusterDistance * viewport.width) / 2;
-        const x = clusterRadius * Math.cos(angle);
-        const y = clusterRadius * Math.sin(angle);
+        const angle = (i / settings.clusters) * 2 * Math.PI;
+        const radius = (settings.distance * scale) / 4;
+        const x = radius * Math.cos(angle);
+        const y = radius * Math.sin(angle);
 
         return (
           <Cluster
             key={i}
+            color={COLORS[i % COLORS.length]}
             position={[x, y, 0]}
+            diameter={settings.diameter * scale} // Scale diameter with viewport size
             nodeCount={settings.nodeCount}
-            color={colors[i % colors.length]}
-            nodeDistance={settings.nodeDistance}
-            nodeSize={settings.nodeSize}
-            minRadius={settings.minRadius}
-            maxRadius={settings.maxRadius}
+            nodeSize={settings.nodeSize * scale} // Scale node size with viewport size
           />
         );
       })}
@@ -163,16 +158,10 @@ function Clusters() {
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-0">
+    <main className="absolute w-full h-full left-0 top-0">
       <Canvas
         frameloop="demand"
-        style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          width: "100%",
-          height: "100%",
-        }}
+        className="absolute w-full h-full left-0 top-0"
       >
         <Clusters />
       </Canvas>
